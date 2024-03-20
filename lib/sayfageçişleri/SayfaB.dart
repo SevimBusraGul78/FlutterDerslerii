b8c01de2b8f63f923e62329e0de36116e8ea0fad
Sevim Büşra <sevimbusra78@gmail>
2024-03-20 12:58:35 +0300

    Geri DÖnüş Tuşlarını Kodlama

--- lib/sayfageçişleri/SayfaB.dart
+++ lib/sayfageçişleri/SayfaB.dart
@@ -10,37 +10,55 @@ class SayfaB extends StatefulWidget {
 }
 
 class _SayfaBState extends State<SayfaB> {
+  Future<bool> geriDonusTusu(BuildContext context) async {
+    print("Geri tusu tıklandı");
+   // return false; //geri dönmesini istemiyorum
+    Navigator.of(context).popUntil((route) => route.isFirst);
+     return true;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("SayfaB"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: (){
+            print("appbar geri tuşu tıklandı");//consolda yazı bastırır
+            Navigator.push(context,MaterialPageRoute(builder:(context) => AnaSayfa()));
+          },
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text("Geldiği Sayfaya Dön"),
-              onPressed: (){
-              Navigator.pop(context); //geldiği sayfaya geri dönüş sağlar
-              },
-            ),
-            ElevatedButton(
-              child: Text("AnaSayfayaGeçişYap"),
-              onPressed: (){
-                Navigator.push(context,MaterialPageRoute(builder:(context) => AnaSayfa()));
-
-              },
-            ),
-            ElevatedButton(
-              child: Text("AnaSayfayadön"), //geri dönüş için sayfa bırakmaz
-              onPressed: (){
-                Navigator.of(context).popUntil((route) => route.isFirst);
-              },
-            ),
-
-          ],
+      body: WillPopScope(
+        onWillPop: () => geriDonusTusu(context),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: Text("Geldiği Sayfaya Dön"),
+                onPressed: (){
+                Navigator.pop(context); //geldiği sayfaya geri dönüş sağlar
+                },
+              ),
+              ElevatedButton(
+                child: Text("AnaSayfayaGeçişYap"),
+                onPressed: (){
+                  Navigator.push(context,MaterialPageRoute(builder:(context) => AnaSayfa()));
+        
+                },
+              ),
+              ElevatedButton(
+                child: Text("AnaSayfayadön"), //geri dönüş için sayfa bırakmaz
+                onPressed: (){
+                  Navigator.of(context).popUntil((route) => route.isFirst);
+                },
+              ),
+        
+            ],
+          ),
         ),
       ),
     );
