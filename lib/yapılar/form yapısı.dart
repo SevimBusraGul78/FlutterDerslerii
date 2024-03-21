8e8023f94c5c3d1209e8eb3ec630eac99f2d34bc
Sevim Büşra <sevimbusra78@gmail>
2024-03-21 14:31:17 +0300

    resimeklendi

--- lib/yapılar/form yapısı.dart
+++ lib/yapılar/form yapısı.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class FormYapisi extends StatelessWidget {
    FormYapisi({super.key});
@@ -13,12 +14,30 @@ class FormYapisi extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.red,
         title: Text("Form Yapısı 2. Kez yazıyorsun dikkatli ol "),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+        ClipOval(
+        child: Container(
+        width: 300,
+          height: 300,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: Colors.red,
+              width: 2.0,
+            ),
+          ),
+          child: Image.asset(
+            "lib/resim/OIG4.jpeg", // Doğru resim yolunu verin
+            fit: BoxFit.cover,
+          ),
+        ),
+        ),
             Padding(
               padding: const EdgeInsets.all(20.0),
               child: Form(key: formKey,
