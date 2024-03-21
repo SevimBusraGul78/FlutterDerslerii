5b054d0c795bc2264efb9ab84cf28517506dd16e
Sevim Büşra <sevimbusra78@gmail>
2024-03-21 17:09:32 +0300

    popMenü

--- lib/sayfageçişleri/main.dart
+++ lib/sayfageçişleri/main.dart
@@ -1,18 +1,23 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import '../yapılar/form yapısı.dart';
 
 void main() {
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
+
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/yapılar/form yapısı.dart
+++ lib/yapılar/form yapısı.dart
@@ -1,95 +1,173 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: FormYapisi(),
+  ));
+}
 
 class FormYapisi extends StatelessWidget {
-   FormYapisi({super.key});
-  var formKey=GlobalKey<FormState>();
-  var tfKullaniciAdi=TextEditingController();
-  var tfSifre=TextEditingController();
+  FormYapisi({Key? key}) : super(key: key);
 
-
+  var formKey = GlobalKey<FormState>();
+  var tfKullaniciAdi = TextEditingController();
+  var tfSifre = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
-        title: Text("Form Yapısı 2. Kez yazıyorsun dikkatli ol "),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Değerli",
+              style: TextStyle(color: Colors.white, fontSize: 16.0),
+            ),
+            Text(
+              "Sevim Büşra",
+              style: TextStyle(color: Colors.white, fontSize: 12.0),
+            ),
+          ],
+        ),
+        centerTitle: false,
+        leading: IconButton(
+          tooltip: "Menu İcon",
+          icon: Icon(Icons.dehaze),
+          onPressed: () {
+            print("menü ikon tıklandı");
+          },
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "çıkış",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          IconButton(
+            tooltip: "Bilgi",
+            icon: Icon(Icons.info_outline, color: Colors.white),
+            onPressed: () {
+              print("bilgi tıklandı");
+            },
+          ),
+          PopupMenuButton(
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                value: 1,
+                child: Text("Sil"),
+              ),
+              PopupMenuItem(
+                value: 2,
+                child: PopupMenuButton(
+                  child: Text("Güncelle"),
+                  itemBuilder: (context) => [
+                    PopupMenuItem(
+                      value: 3,
+                      child: Text("Seçenek 1"),
+                    ),
+                    PopupMenuItem(
+                      value: 4,
+                      child: Text("Seçenek 2"),
+                    ),
+                  ],
+                  onSelected: (menuItemValue) {
+                    if (menuItemValue == 3) {
+                      print("Güncelle - Seçenek 1 tıklandı");
+                    } else if (menuItemValue == 4) {
+                      print("Güncelle - Seçenek 2 tıklandı");
+                    }
+                  },
+                ),
+              ),
+            ],
+            onSelected: (menuItemValue) {
+              if (menuItemValue == 1) {
+                print("Sil tıklandı");
+              }
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-        ClipOval(
-        child: Container(
-        width: 300,
-          height: 300,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(
-              color: Colors.red,
-              width: 2.0,
+            ClipOval(
+              child: Container(
+                width: 300,
+                height: 300,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.red,
+                    width: 2.0,
+                  ),
+                ),
+                child: Image.asset(
+                  "lib/resim/OIG4.jpeg", // Doğru resim yolunu verin
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-          ),
-          child: Image.asset(
-            "lib/resim/OIG4.jpeg", // Doğru resim yolunu verin
-            fit: BoxFit.cover,
-          ),
-        ),
-        ),
             Padding(
               padding: const EdgeInsets.all(20.0),
-              child: Form(key: formKey,
-               child:Column(
-                 children: [
-                   TextFormField(
-                     controller:tfKullaniciAdi,
-                     decoration: InputDecoration(hintText: "Kullanici Adi"),
-                     validator:(tfgirdisi){
-                       if(tfgirdisi!.isEmpty){
-                         return "Kullanici Adlini Giriniz";
-                       }
-                       return null ;
-                     },
-                   ),
-                   TextFormField(
-                     controller:tfSifre,
-                     obscureText: true, //şife girdiğimizde karakteri saklasın
-                     decoration: InputDecoration(hintText: "sifre"),
-                     validator:(tfgirdisi){
-                       if(tfgirdisi!.isEmpty){
-                         return "Sifre Giriniz";
-                       }
-                       if(tfgirdisi!.length<6){
-                         return "Sifreniz en aaz 6 haneli olmalıdır ";
-                       }
-                       return null ;
-                     },
-                   ),
-                   SizedBox(
-                     height: 80,
-                     width: 200,
-
-                     child: Padding(
-                       padding: const EdgeInsets.all(15.0),
-                       child: ElevatedButton(onPressed: (){
-                         print("girildi");
-                         bool kontrolSonucu=formKey.currentState!.validate();
-                         if(kontrolSonucu){
-                           String ka=tfKullaniciAdi.text;
-                           String kb=tfSifre.text;
-                           print("kullanici adi :$ka- kullanıcı şifre :$kb");
-                         }
-                         },
-                         style: ElevatedButton.styleFrom(
-                             foregroundColor: Colors.white, backgroundColor: Colors.red,), // İçerik (text) rengi
-                             // Butonun diğer özelliklerini de buraya ekleyebilirsiniz
-                        child: Text("Başla")),
-                     ),
-                   )
-                 ],
-               )
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: tfKullaniciAdi,
+                      decoration: InputDecoration(hintText: "Kullanıcı Adı"),
+                      validator: (tfgirdisi) {
+                        if (tfgirdisi!.isEmpty) {
+                          return "Kullanıcı Adını Giriniz";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: tfSifre,
+                      obscureText: true, // şifre girdiğimizde karakteri saklasın
+                      decoration: InputDecoration(hintText: "Şifre"),
+                      validator: (tfgirdisi) {
+                        if (tfgirdisi!.isEmpty) {
+                          return "Şifre Giriniz";
+                        }
+                        if (tfgirdisi.length < 6) {
+                          return "Şifreniz en az 6 haneli olmalıdır";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 80,
+                      width: 200,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            print("girildi");
+                            bool kontrolSonucu =
+                            formKey.currentState!.validate();
+                            if (kontrolSonucu) {
+                              String ka = tfKullaniciAdi.text;
+                              String kb = tfSifre.text;
+                              print("Kullanıcı Adı: $ka - Şifre: $kb");
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white, backgroundColor: Colors.red,
+                          ),
+                          child: Text("Başla"),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             )
           ],
